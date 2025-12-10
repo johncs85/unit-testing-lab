a9854cb0d31e99fa0084fa6b6673a190d89fb69e
johncs85 <johncshear85@gmail.com>
2025-12-09 20:40:56 -0500

    Create react add todo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import { useState } from "react";
+import { useState } from "react"
 
 const App = () => {
+  const [todo, setTodo] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const handleAddTodo = () => {
+    if (todo.trim() === "") return
+
+    setTodos([...todos, todo])
+    setTodo("")
+  }
+
   return(
     <>
-      <h1>Hello, World!</h1>
+       <h1>Todo App</h1>
+
+      <input
+        placeholder="Enter a todo"
+        value={todo}
+        onChange={(event) => setTodo(event.target.value)}
+      />
+
+      <button onClick={handleAddTodo}>Add Todo</button>
+
+      <ul>
+        {todos.map((todo, id) => (
+          <li key={id}>{todo}</li>
+        ))}
+      </ul>
     </>
   )
 }
 
-export default App;
+export default App
